a0e86e1848bd07f7ba720fac18d3f08b0df94386
Unknown <verner@fortelius.net>
2018-09-22 13:10:09 +0300

    refactoring

--- StripeGatewayFunction.cs
+++ StripeGatewayFunction.cs
@@ -17,18 +17,43 @@ namespace StripeGatewayFunction
         private const String VaultUrl = "https://flexinetsbilling.vault.azure.net/";
         private static String _fortnoxAccessToken;
         private static String _fortnoxClientSecret;
+        private static ILogger _log;
 
 
         [FunctionName("StripeGateway")]
         public async static Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequest req, ILogger log)
         {
+            _log = log;
             var stripeEvent = StripeEventUtility.ParseEvent(await req.ReadAsStringAsync());
             log.LogInformation($"Received stripe event of type: {stripeEvent.Type}");
 
-            var keyvault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
 
-            // todo this is maybe not always the case... but good enough for now
-            if (stripeEvent.LiveMode)
+            await LoadSettingsAsync(stripeEvent.LiveMode);  // todo this is maybe not always the case... but good enough for now
+
+            if (stripeEvent.Type == StripeEvents.CustomerCreated)
+            {
+                await HandleCustomerCreatedAsync(stripeEvent);
+
+            }
+            else if (stripeEvent.Type == StripeEvents.ChargeSucceeded)
+            {
+                await HandleChargeSucceededAsync(stripeEvent);
+            }
+
+
+            return new OkResult();
+        }
+
+
+        /// <summary>
+        /// Much side effect such sad
+        /// </summary>
+        /// <param name="production"></param>
+        /// <returns></returns>
+        public async static Task LoadSettingsAsync(Boolean production)
+        {
+            var keyvault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));            
+            if (production)
             {
                 _fortnoxAccessToken = (await keyvault.GetSecretAsync(VaultUrl, "fortnox-access-token-prod")).Value;
                 _fortnoxClientSecret = (await keyvault.GetSecretAsync(VaultUrl, "fortnox-client-secret-prod")).Value;
@@ -38,51 +63,60 @@ namespace StripeGatewayFunction
                 _fortnoxAccessToken = (await keyvault.GetSecretAsync(VaultUrl, "fortnox-access-token-test")).Value;
                 _fortnoxClientSecret = (await keyvault.GetSecretAsync(VaultUrl, "fortnox-client-secret-test")).Value;
             }
+        }
 
 
-            if (stripeEvent.Type == StripeEvents.CustomerCreated)
+        /// <summary>
+        /// Create customer in fortnox
+        /// </summary>
+        /// <param name="stripeEvent"></param>
+        /// <returns></returns>
+        public async static Task HandleCustomerCreatedAsync(StripeEvent stripeEvent)
+        {
+            var stripeCustomer = Mapper<StripeCustomer>.MapFromJson((String)stripeEvent.Data.Object.ToString());
+
+            var customer = new
             {
-                var stripeCustomer = Mapper<StripeCustomer>.MapFromJson((String)stripeEvent.Data.Object.ToString());
-
-                var customer = new
+                Customer = new
                 {
-                    Customer = new
+                    Address1 = stripeCustomer.Shipping.Address.Line1,
+                    City = stripeCustomer.Shipping.Address.City,
+                    CountryCode = stripeCustomer.Shipping.Address.Country,
+                    Currency = "EUR",
+                    CustomerNumber = stripeCustomer.Id,
+                    Email = stripeCustomer.Email,
+                    EmailInvoice = stripeCustomer.Email,
+                    InvoiceRemark = "DO NOT PAY THIS INVOICE!\n\nYOU HAVE PREPAID BY CREDIT/DEBIT CARD.",
+                    Name = stripeCustomer.Shipping.Name,
+                    YourReference = stripeCustomer.Shipping.Name,
+                    ZipCode = stripeCustomer.Shipping.Address.PostalCode,
+                    OurReference = "web",
+                    TermsOfPayment = "K",
+                    VATType = "EXPORT",
+                    VATNumber = stripeCustomer.TaxInfo?.TaxId,
+                    DefaultDeliveryTypes = new
                     {
-                        Address1 = stripeCustomer.Shipping.Address.Line1,
-                        City = stripeCustomer.Shipping.Address.City,
-                        CountryCode = stripeCustomer.Shipping.Address.Country,
-                        Currency = "EUR",
-                        CustomerNumber = stripeCustomer.Id,
-                        Email = stripeCustomer.Email,
-                        EmailInvoice = stripeCustomer.Email,
-                        InvoiceRemark = "DO NOT PAY THIS INVOICE!\n\nYOU HAVE PREPAID BY CREDIT/DEBIT CARD.",
-                        Name = stripeCustomer.Shipping.Name,
-                        YourReference = stripeCustomer.Shipping.Name,
-                        ZipCode = stripeCustomer.Shipping.Address.PostalCode,
-                        OurReference = "web",
-                        TermsOfPayment = "K",
-                        VATType = "EXPORT",
-                        VATNumber = stripeCustomer.TaxInfo?.TaxId,
-                        DefaultDeliveryTypes = new
-                        {
-                            Invoice = "EMAIL"
-                        }
+                        Invoice = "EMAIL"
                     }
-                };
-
-                var result = await CreateFortnoxHttpClient().PostAsJsonAsync("https://api.fortnox.se/3/customers/", customer);
-                if (!result.IsSuccessStatusCode)
-                {
-                    log.LogInformation(await result.Content.ReadAsStringAsync());
                 }
-            }
-            else if (stripeEvent.Type == StripeEvents.ChargeSucceeded)
+            };
+
+            var result = await CreateFortnoxHttpClient().PostAsJsonAsync("https://api.fortnox.se/3/customers/", customer);
+            if (!result.IsSuccessStatusCode)
             {
-                // todo do something useful
+                _log.LogError(await result.Content.ReadAsStringAsync());
             }
+        }
 
 
-            return new OkResult();
+        /// <summary>
+        /// Create order in fortnox
+        /// </summary>
+        /// <param name="stripeEvent"></param>
+        /// <returns></returns>
+        public async static Task HandleChargeSucceededAsync(StripeEvent stripeEvent)
+        {
+
         }
 
 
